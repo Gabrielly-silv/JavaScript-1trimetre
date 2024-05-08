35c72ff4aa1195030d8bdcbba0625f19c87128a0
Gabrielly-silv <163124582+Gabrielly-silv@users.noreply.github.com>
2024-05-08 12:01:08 -0300

    Add files via upload

--- desafio7.js
+++ desafio7.js
@@ -6,5 +6,4 @@ const listaDeAlunosEMedias = [alunos,medias]
 console.log(
     `O aluno da posição 1 da lista de alunos é:${listaDeAlunosEMedias[0][1]}
      A nota desse aluno é ${listaDeAlunosEMedias[1][1]}`
-   
 );
