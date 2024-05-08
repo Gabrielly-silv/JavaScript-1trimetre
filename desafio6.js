667987542fbc584ca09f2896fbcc6743e9797f59
Gabrielly-silv <163124582+Gabrielly-silv@users.noreply.github.com>
2024-05-08 12:00:21 -0300

    Add files via upload

---
+++ desafio6.js
@@ -0,0 +1,8 @@
+const salaJS = ["Emilia","Luis","Gloria"];
+const salaPython = ["Joana","Mel","Carlos"];
+
+const SalasUnificadas = salaJS.concat(salaPython);
+const SalasUnificadas2 = salaPython.concat(salaJS);
+
+console.log (SalasUnificadas)
+console.log (SalasUnificadas2)
---
+++ desafio7.js
@@ -0,0 +1,10 @@
+const alunos = ["Laura","pedro","João","hannah"];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos,medias]
+
+console.log(
+    `O aluno da posição 1 da lista de alunos é:${listaDeAlunosEMedias[0][1]}
+     A nota desse aluno é ${listaDeAlunosEMedias[1][1]}`
+   
+);
